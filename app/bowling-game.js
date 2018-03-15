ad1e997269fa4ba541b348cde2f346370a5d3cf8
Michelle Sinn <sinn.mk@gmail.com>
2018-03-15 14:14:20 -0500

    bowling game kata in javascript, app and test

--- app/bowling-game.js
+++ app/bowling-game.js
@@ -1,8 +1,56 @@
+function sum(arr) {
+  return arr.reduce((sum, x) => sum + x)
+}
+
 class BowlingGame {
 
-  returnOne() {
-    return 1
+  constructor(){
+    this.rolls = []
+    this.currentRoll = 0
+  }
+  
+  roll(pins){
+    this.rolls[this.currentRoll] = pins
+    this.currentRoll++
+  }
+
+  scoreGame(){  
+    var frameIndex = 0
+    var score = 0
+    for (var frame = 0; frame < 10; frame++){
+      if(this.isStrike(frameIndex)){
+        score += 10 + this.strikeBonus(frameIndex) 
+        frameIndex++
+      } else if (this.isSpare(frameIndex)){
+        score += 10 + this.spareBonus(frameIndex) 
+        frameIndex += 2
+      } else {
+        score += this.sumOfBallsInFrame(frameIndex) 
+        frameIndex += 2
+      }
+    }
+    return score
+  }
+
+  isSpare(frameIndex){
+    return this.rolls[frameIndex] + this.rolls[frameIndex+1] == 10
+  }
+
+  spareBonus(frameIndex){
+    return this.rolls[frameIndex + 2]
+  }
+
+  isStrike(frameIndex){
+    return this.rolls[frameIndex] == 10
+  }
+
+  strikeBonus(frameIndex){
+    return this.rolls[frameIndex+1] + this.rolls[frameIndex+2]
+  }
+
+  sumOfBallsInFrame(frameIndex){
+    return this.rolls[frameIndex] + this.rolls[frameIndex+1]
   }
 }
 
-module.exports = new BowlingGame();
+module.exports = BowlingGame;
--- test/bowling-game.js
+++ test/bowling-game.js
@@ -1,10 +1,56 @@
-let expect = require("chai").expect;
-let primes = require("../app/bowling-game");
+let expect = require("chai").expect; let BowlingGame = require("../app/bowling-game");
+let game;
 
-describe("Bowling Game", () => {
+describe("BowlingGame", function() {
 
-  it("Returns 1", () => {
-    expect(primes.returnOne()).equal(1)
+  beforeEach( function(){
+    game = new BowlingGame();
   });
 
+  function rollMany(n, pins){
+    for (var i = 0; i < n; i ++){
+      game.roll(pins)
+    }
+  }
+
+  function rollSpare(){
+    game.roll(5)
+    game.roll(5)
+  }
+
+  function rollStrike(){
+    game.roll(10)
+  }
+
+
+  it("can score a gutter game", function() {
+    rollMany(20, 0)
+    expect(game.scoreGame()).to.equal(0);
+  });
+
+  it("can score a game of all ones", function() {
+    rollMany(20,1)
+    expect(game.scoreGame()).to.equal(20);
+  });
+
+  it("can score a spare", function() {
+    rollSpare()
+    game.roll(3)
+    rollMany(17, 0)
+    expect(game.scoreGame()).to.equal(16);
+  });
+
+  it("can score a strike", function() {
+    rollStrike()
+    game.roll(3)
+    game.roll(4)
+    rollMany(16, 0)
+    expect(game.scoreGame()).to.equal(24);
+  });
+
+    var playGame = function(pin){
+     for(var x = 20; x >= 1; x-=1){
+       game.roll(pin);
+     }
+   };
 });
